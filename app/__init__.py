09417104a646cf0c9d5077dd0fb71ef1b1a945fa
Nicolas Rey <nicolas.rey.pro38@gmail.com>
2026-04-20 15:28:33 +0200

    Fix static_url_path pour déploiement sous /dyneco/

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from config import Config
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__,
+                static_url_path='/dyneco/static',
+                static_folder='static')
     app.config.from_object(Config)
 
     from app.routes import main
-    app.register_blueprint(main)
+    app.register_blueprint(main, url_prefix='')
 
     return app
